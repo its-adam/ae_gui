d97475ad0d4cd618736436be66f496576615907b
its-adam <32000089+its-adam@users.noreply.github.com>
2017-09-18 12:55:40 -0700

    Create ali_web.py
    populate with template

---
+++ ali_web.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  ali_web.py
+#
+import json
+from urllib.request import urlopen
+import re
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
